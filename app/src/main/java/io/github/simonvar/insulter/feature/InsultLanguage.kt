1b587a094d23ff77932fbf601150cc2269ca2879
Yaroslav Atroshenko <yaaroslavok@gmail.com>
2019-03-20 20:57:38 +0300

    add insult language

---
+++ app/src/main/java/io/github/simonvar/insulter/feature/InsultLanguage.kt
@@ -0,0 +1,5 @@
+package io.github.simonvar.insulter.feature
+
+enum class InsultLanguage {
+    RU, EN, DE, FR
+}
--- app/src/main/java/io/github/simonvar/insulter/feature/data/InsultState.kt
+++ app/src/main/java/io/github/simonvar/insulter/feature/data/InsultState.kt
@@ -1,6 +1,12 @@
 package io.github.simonvar.insulter.feature.data
 
+import android.os.Parcelable
+import io.github.simonvar.insulter.feature.InsultLanguage
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class InsultState(
     val text: String?,
-    val isLoading: Boolean
-)
+    val isLoading: Boolean,
+    val lang: InsultLanguage
+) : Parcelable
